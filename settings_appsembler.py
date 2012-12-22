a15b2e76f2e046fca4d3a888778d98844bc67ddf
Nate Aune <natea@jazkarta.com>
2012-12-22 17:26:17 -0600

    use the new collectstatic feature to serve up the static assets rather than django.staticserve

--- settings_appsembler.py
+++ settings_appsembler.py
@@ -1,5 +1,5 @@
 import os.path
-from mediathread.settings import *
+from settings import *
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.auth',
@@ -8,14 +8,34 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'mediathread_main.views.django_settings',
     )
 
+EXTRA_INSTALLED_APPS = (
+    'django.contrib.staticfiles',
+    )
+
+# add any extra apps to the INSTALLED_APPS
+try:
+    INSTALLED_APPS = tuple(list(INSTALLED_APPS) + list(EXTRA_INSTALLED_APPS))
+except NameError:
+    pass
+
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_ROOT, "templates"),
 )
 
+# original media assets are copied from the media directory
+STATICFILES_DIRS = (
+     os.path.join(PROJECT_ROOT, 'media/'),
+)
+
+# static media is collected to the directory site_media
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "site_media")
+STATIC_URL = '/site_media/'
+
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, "uploads")
 MEDIA_URL = '/uploads/' # same as in settings_shared.py, just to keep both settings together
+
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
